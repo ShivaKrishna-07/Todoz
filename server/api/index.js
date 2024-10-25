478d7d91af64cbdb4b669e1e0ae206137cc3bb79
Shiva Krishna <shivakrishna6032@gmail.com>
2024-10-25 23:24:49 +0530

    update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import { Connection } from './database/db.js';
-import Router from './routes/route.js';
+import { Connection } from '../database/db.js';
+import Router from '../routes/route.js';
     
 
 const app = express();
